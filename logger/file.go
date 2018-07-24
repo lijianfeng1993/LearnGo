07be812c6e4f987d269dae89295732b2d4923260
lijianfeng1993 <lijianfeng@cmss.chinamobile.com>
2018-07-24 15:02:52 +0800

    remove func:writelog from file.go

--- logger/file.go
+++ logger/file.go
@@ -51,23 +51,6 @@ func (f *FileLogger) SetLevel(level int){
 	f.level = level
 }
 
-func (f *FileLogger) writeLog(file *os.File, level int, format string, args ...interface{}){
-	if (f.level) > level {
-		return
-	}
-
-	now := time.Now()
-	nowStr := now.Format("2006-01-02 15:04:05.999")  //以go的诞生之日为时间点格式化，固定的，不能变
-
-	levelStr := getLevelText(level)
-
-	fileName, funcName, lineNo := GetLineInfo()
-	fileName = path.Base(fileName)
-	funcName = path.Base(funcName)
-
-	msg := fmt.Sprintf(format, args...)
-	fmt.Fprintf(file, "%s %s (%s:%s:%d) %s\n", nowStr, levelStr, fileName, funcName, lineNo, msg)
-}
 
 func (f *FileLogger) Debug(format string, args ...interface{}){
 	if f.level > LogLevelDebug {
